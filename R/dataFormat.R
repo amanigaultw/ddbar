aaa9bbe75cddc34492e05caafb00b2484a0b3c92
amanigaultw <andrewmanigaultw@gmail.com>
2023-02-17 08:48:07 -0500

    work in progress saving for the sake of backing up online

--- R/dataFormat.R
+++ R/dataFormat.R
@@ -1,41 +1,72 @@
-devtools::install_github("amanigaultw/ddbar")
-
-library(ddbar)
-
-
-
+rm(list = ls())
 
 rawdata <- data.frame(nationality = sample(c("French", "German", "British"), 100, replace=TRUE, prob=c(0.4, 0.3, 0.3)),
                       sex = sample(c("Male", "Female"), 100, replace=TRUE, prob=c(0.5, 0.5)),
                       age = sample(c("child", "adult", "older adult"), 100, replace=TRUE, prob=c(0.1, 0.7, 0.2)))
 
-data <- data.frame(nationality = sample(c("French", "German", "British"), 100, replace=TRUE, prob=c(0.4, 0.3, 0.3)))
+firstNonUniqueCol <- function(data){
+  for(i in 1:ncol(data)){
+    if(length(unique(data[,i])) == 1) next
+    return(i)
+  }
+}
+
+isTerminal <- function(data){
+  if(is.null(firstNonUniqueCol(data))) return(TRUE)
+  return(FALSE)
+}
+
+getDataList <- function(rawdata){
+  todo <- list(rawdata)
+  done <- list()
+  while(length(todo) > 0){
+    for(i in seq_along(todo)){
+      data <- todo[[i]]
+      done <- append(done, list(data))
+      col = firstNonUniqueCol(data)
+      for(j in seq_along(unique(data[,col]))){
+        todo <- append(todo, list(data[data[,col] == unique(data[,col])[j],]))
+      }
+    }
+    todo <- todo[!todo %in% done]
+  }
+  done
+}
+
+dataList <- getDataList(rawdata)
+
+
+data <- dataList[[2]]
+
+getDataGroupId <- function(data){
+  if(firstNonUniqueCol(data) == 1){
+    return("root")
+  }
+  else{
+    col <- firstNonUniqueCol(data)
+    for(i in seq_along(1:(col - 1))){
+      temp <- apply(data[,1:i], 1, function(x) paste(x, collapse = "-"))
+    }
+  }
+}
 
 
 
-foo <- function(data){
 
-  temp <- table(data[,1])
 
-  as.list(c(names(temp)))
+toeChartListFormat <- function(data){
 
-  list(dataGroupId = "root",
-       data = list(c("animals", 5, "animals"),
-                   c("fruits", 2, "fruits")))
 
 }
 
-temp <- list()
-for(i in seq_along(unique(rawdata[,1]))){
-  subdata = rawdata[rawdata[,1] == unique(rawdata[,1])[i], -1]
-  temp[[i]] <- list(count = nrow(subdata),
-                    subdata = subdata)
-  names(temp)[i] <- unique(rawdata[,1])[i]
-}
 
 
 
 
+
+devtools::install_github("amanigaultw/ddbar")
+
+library(ddbar)
 
 
 data = list(list(dataGroupId = "1",
